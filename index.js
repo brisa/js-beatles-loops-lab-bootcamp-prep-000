e88bab1c223ce70650406aee4516b3056999c8e4
Brisa Carter <brisa.carter@gmail.com>
2019-05-16 17:48:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+let emptyArr = []
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+  for(let i = 0; i < arrayMusicians.length; i++){
+      emptyArr.push(arrayMusicians[i] + " plays "+ arrayInstruments[i])
+  }
+  
+  return(emptyArr)
+}
+let newFacts = []
+function johnLennonFacts(arrayFacts){
+  for(let i = 0; i<arrayFacts; i++){
+    newFacts.push(arrayFacts[i]+ "!!!")
+    return newFacts
+  }
+}
